c8200c7a0e6e17d0756aaf2a1a7bec19dfbc3e7b
Rutuja Naik <rutujanaik2007@gmail.com>
2025-04-29 22:32:15 +0530

    Create Greater than 3.c

---
+++ Greater than 3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("Enter the number");
+    scanf("%d",& number);
+    
+    if(number>3)
+    {
+        printf("yes");
+    }
+    printf("End of program");
+    
+    return 0;
+    
+  
