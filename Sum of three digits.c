98070a5ebe88a1a8f0567f79108d5616995bd014
Rutuja Naik <rutujanaik2007@gmail.com>
2025-05-04 23:19:09 +0530

    Create Sum of three digits.c

---
+++ Sum of three digits.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int num,rem1,rem2,rem3,q1,q2,total;
+    
+    printf("Enter the three digit number\t");
+    scanf("%d",&num);
+    
+    rem1=num%10;
+    q1=num/10;
+    
+    rem2=num%10;
+    q1=num/10;
+    
+    rem3=q2%10;
+    total=rem1+rem2+rem3;
+    
+    printf("The sum of three digit entered is %d",total );
+   
+   return 0;
+}￼Enter
