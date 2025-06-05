79307b6b55e1597b27db5eef81cc638b2fd34ac5
Rutuja Naik <rutujanaik2007@gmail.com>
2025-06-05 21:27:14 +0530

    Create array star to numbers.c

---
+++ array star to numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i=5,j;
+    for(i=0;i<5;i++)
+    {
+        for(j=0;j<=i;j++)
+    {
+        printf("%d",i);
+    }
+    printf("\n");
+    }
+    return 0;
+    }
